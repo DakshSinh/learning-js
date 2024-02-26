fc330a82cc89dfe4427c301c23053491e9d19fa3
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-26 01:40:34 +0000

    datatype conversion

---
+++ basics_js/conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "A"
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valInNum = Number(score);
+console.log(typeof valInNum);
+console.log(valInNum);
+
+/*
+    "1" -> 1
+    "A" -> NaN
+    trur -> 1 false -> 0
+*/
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof(booleanIsLoggedIn));
+console.log(booleanIsLoggedIn);
+
+let aNum = 100
+let aNumString = String(aNum)
+
+console.log(typeof(aNumString));
+console.log(aNumString);
