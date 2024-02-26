afc894c69ede5b11f058fece53a6ef3a0768f1ea
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-26 01:22:30 +0000

    variables and datatypes

---
+++ basics_js/dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// alert() does not work as we use NodeJs
+// primitive -> number, bigint, string, boolean, null, undefined, symbol
+
+// * null is a object
+
+console.log(typeof undefined);
+
---
+++ basics_js/variables.js
@@ -0,0 +1,14 @@
+const accountId = 1;
+let accountEmail = "abc@gmail.com";
+var accountpassword = "1234";
+accountCity = "Surat";
+let isVal;
+
+console.table([accountId, accountEmail, accountpassword, accountCity, isVal]);
+
+// accountId = 2
+accountEmail = "xyz@gmail.com";
+accountpassword = "4321";
+accountCity = "Baroda";
+
+console.table([accountId, accountEmail, accountpassword, accountCity]);
