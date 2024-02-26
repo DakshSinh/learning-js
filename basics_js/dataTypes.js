66904f5305c5f65c768dff971653b358b57cf93d
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-26 02:55:10 +0000

    more on datatypes

--- basics_js/dataTypes.js
+++ basics_js/dataTypes.js
@@ -7,27 +7,48 @@
 /*
     primitive -> number, bigint, string, boolean, null, undefined, symbol
     -> call by value
+    -> Stack memory used (copy of data is provided)
 */
 /*
     non-primitive -> array, objects, functions
     -> call by refrence
+    -> heap memory used (refrence of original value)
 */
 
-const id = Symbol("123");
-const id2 = Symbol("123");
+// const id = Symbol("123");
+// const id2 = Symbol("123");
 
-console.log(id === id2);
+// console.log(id === id2);
 
-const heros = ["daksh", "poojan", "nitya"];
+// const heros = ["daksh", "poojan", "nitya"];
 
-let myInfo = {
-  name: "daksh",
-  age: 19,
+// let myInfo = {
+//   name: "daksh",
+//   age: 19,
+// };
+
+// const myFunc = function () {
+//   console.log("My Function");
+// };
+
+// console.log([myFunc, myInfo, heros]);
+// console.log(myInfo);
+
+let uName = "Daksh";
+let tempName = uName;
+
+tempName = "Not Daksh";
+
+console.log(tempName);
+console.log(uName);
+
+let user1 = {
+  email: "demo.com",
+  upi: "demo_upi",
 };
 
-const myFunc = function () {
-  console.log("My Function");
-};
+let user2 = user1;
+user2.email = "test.com";
 
-console.log([myFunc, myInfo, heros]);
-console.log(myInfo);
+console.log(user1.email);
+console.log(user2.email);
