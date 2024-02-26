b650bff41086293b93d5801424463864770a08e7
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-26 02:39:11 +0000

    more on data types

--- basics_js/dataTypes.js
+++ basics_js/dataTypes.js
@@ -1,9 +1,33 @@
 "use strict";
 
 // alert() does not work as we use NodeJs
-// primitive -> number, bigint, string, boolean, null, undefined, symbol
+// null is a object
+// js is dynamically typed
 
-// * null is a object
+/*
+    primitive -> number, bigint, string, boolean, null, undefined, symbol
+    -> call by value
+*/
+/*
+    non-primitive -> array, objects, functions
+    -> call by refrence
+*/
 
-console.log(typeof undefined);
+const id = Symbol("123");
+const id2 = Symbol("123");
 
+console.log(id === id2);
+
+const heros = ["daksh", "poojan", "nitya"];
+
+let myInfo = {
+  name: "daksh",
+  age: 19,
+};
+
+const myFunc = function () {
+  console.log("My Function");
+};
+
+console.log([myFunc, myInfo, heros]);
+console.log(myInfo);
