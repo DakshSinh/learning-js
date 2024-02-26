0d01dae4adce7e7df8e05a2b1bd30d6bdca56433
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-26 05:40:06 +0000

    learning numbers and math

---
+++ basics_js/number.js
@@ -0,0 +1,26 @@
+// const score = 100
+// const bal = new Number(100)
+
+// console.log(bal, score);
+// console.log(`Score: ${score} and Balance: ${bal}`);
+
+// console.log(bal.toFixed(2));
+
+// const aNumber = 100.123456789
+// console.log(aNumber.toPrecision(1));
+
+// const hunods = 10000000000
+// console.log(hunods.toLocaleString('en-IN'));
+// console.log(Number.MAX_SAFE_INTEGER.toLocaleString('en-IN'));
+
+// Math
+
+// console.log(Math.floor(4.6));
+// console.log(Math.round((Math.random()*10) + 1));
+
+// imp
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
