99d35d36689959212d702b62f360b1c18d882e4b
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-26 02:17:41 +0000

    comparisions

---
+++ basics_js/comparision.js
@@ -0,0 +1,7 @@
+console.log("3" > "2");
+console.log(null > 0);
+console.log(null >= 0);
+
+// === -> checks datatype too
+
+console.log("1" === 1);
--- basics_js/conversionOperation.js
+++ basics_js/conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "A"
+let score = "A";
 
 console.log(typeof score);
 console.log(typeof score);
@@ -13,14 +13,45 @@ console.log(valInNum);
     trur -> 1 false -> 0
 */
 
-let isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(typeof(booleanIsLoggedIn));
+console.log(typeof booleanIsLoggedIn);
 console.log(booleanIsLoggedIn);
 
-let aNum = 100
-let aNumString = String(aNum)
+let aNum = 100;
+let aNumString = String(aNum);
 
-console.log(typeof(aNumString));
+console.log(typeof aNumString);
 console.log(aNumString);
+
+// Operations +, -, /, %, *, **(power)
+
+let val = 1
+let negVal = -val
+console.log(negVal);
+
+let str1 = "Hi, "
+let str2 = "Daksh"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log(typeof console.log("1" + 2));
+console.log(typeof console.log(1 + "2"));
+console.log(typeof console.log((1 + 2) + "2"));
+console.log(typeof console.log((1 + "2") + 2));
+
+console.log(+"");
+
+let no1, no2, no3 = 2 + 2
+
+let x = 1
+console.log(x++);
+let y = x
+console.log(y);
+
+let a = 1
+console.log(++a);
+let b = a
+console.log(b);
