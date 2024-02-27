4740ceb760747306fe8ba3a04c605298ec7c4b44
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-27 22:19:52 +0000

    learning functions

---
+++ basics_js/functions.js
@@ -0,0 +1,44 @@
+// 1
+
+// function demo(a, b) {
+//     return a + b
+// }
+
+// const result = demo(10, 20)
+
+// function dispMessage(username = "uName") {
+//     if (!username) {
+//         console.log("Enter a username");
+//         return 
+//     }
+//     return `${username}, just logged in`
+// }
+// console.log(dispMessage());
+
+// 2 
+// Rest operator and Spread operator
+
+// function calculateCartTotal(val1, val2, ...num) {
+//     return val1, val2, num
+// }
+
+// console.log(calculateCartTotal(1,2,100,200,300));
+
+// Functions with objects & arrays
+
+// const user = {
+//     name: "Daksh",
+//     price: "Free"
+// }
+
+// const arr = [1, 2, 3, 4]
+
+// function whatUser(userDetails, anArr) {
+//     const { name } = userDetails
+//     const { price } = userDetails
+//     console.log(`Username -> ${name} Price -> ${price} Array -> ${anArr[0]}`
+//     );
+// }
+
+// whatUser(user, arr)
+
