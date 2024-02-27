a7225e90b9145764150f6809562cdf331767d34d
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-27 22:53:25 +0000

    learning scopes

---
+++ basics_js/scopes.js
@@ -0,0 +1,41 @@
+// 1
+// Scopes {}
+
+// let a = 100
+
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// Nested functions
+
+// function one() {
+//     const name = "Daksh"
+
+//     function two() {
+//         const price = 10
+
+//         console.log(name);
+//     }
+//     // console.log(price);
+//     two()
+// }
+
+// one()
+
+// 2
+
+// console.log(addOne(11));
+// function addOne(num){
+//     return num + 1
+// }
+
+// // Declaration hoisting error
+// console.log(addTwo(11));
+// const addTwo = function(num){
+//     return num + 2
+// }
