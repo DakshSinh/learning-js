407f786cf484d8a015bcf4e936bf36658d58c5c8
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-27 00:27:09 +0000

    learning arrays

---
+++ basics_js/arrays.js
@@ -0,0 +1,58 @@
+const arr = [0, 1, 2, 3]
+const strArr = ["A", "B", "C"]
+// const aArr = new Array(1, 2, 3)
+
+// console.log(arr[0]);
+// console.log(strArr[0]);
+
+// Array methods
+
+// arr.push("Demo")
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// arr.unshift(-1)
+// arr.shift()
+// console.log(arr);
+
+// const newArr = arr.join()
+// console.log(newArr);
+
+// console.log(arr);
+// const aArr = arr.slice(1, 3)
+// console.log(aArr);
+
+// console.log(arr);
+// const bArr = arr.splice(1, 3)
+// console.log(bArr);
+
+
+// Join array
+
+const hero = ["daksh", "nitya"]
+const villan = ["poojan", "nispand"]
+const inArr = [1, 2, 3, [1, 2, 3, [1, 2, 3]]] 
+
+// const inArrSolver = inArr.flat(Infinity)
+// console.log(inArrSolver);
+
+// hero.push(villan)
+// console.log(hero[2][1]);
+
+// const newArr = hero.concat(villan)
+// console.log(newArr);
+
+// const people = [...hero, ...villan]
+// console.log(people);
+
+console.log(Array.isArray("Daksh")); 
+console.log(Array.from("Daksh"));
+
+console.log(Array.from({
+    name: "Daksh"
+})); 
+
+let a = 1
+let b = 2
+let c = 3
