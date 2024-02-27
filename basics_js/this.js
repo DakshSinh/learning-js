ebb4a3d0b239c65a774ae3fbda5aa6c35cef420a
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-27 23:17:26 +0000

    learning this and arrow operator

---
+++ basics_js/this.js
@@ -0,0 +1,46 @@
+// 1
+
+// const a = 20
+// const user = {
+//     name: "Daksh",
+//     price: 1000,
+//     welcome: function() {
+//         console.log(`Welcome ${this.name}`);
+//         console.log(this);
+//     }
+// }
+
+// console.log(this);
+
+// function demo() {
+//     let c = 10;
+//     console.log(this);
+// }
+
+// demo();
+
+// 2
+// Arrow function
+
+// const demo = function () {
+//     console.log(this);
+// }
+
+// const demo = () => {
+//     console.log(this);
+// }
+
+// demo();
+
+// Simple -> return in nesscessary
+
+// const num = (num1, num2) => {
+//     return num1 + num2;
+// }
+// console.log(num(10, 20));
+
+// Implicit return -> return is not nesscessary
+
+// const num = (num1, num2) => (num1 + num2)
+
+// console.log(num(10, 20));
