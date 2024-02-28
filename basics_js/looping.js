98397945c1612c15011c8f08a6b56802d9c27424
Dakshsinh Parmar <dakshsinhparmar.co22d2@scet.ac.in>
2024-02-28 03:36:52 +0000

    learning looping and maps

---
+++ basics_js/looping.js
@@ -0,0 +1,80 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// let arr = ["apple", "banana", "cherry"];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// Break and continue
+
+// for (let index = 0; index < 20; index++) {
+//     console.log(`Index: ${index}`);
+//     break;
+// }
+
+// const arr = [{ name: "apple" }, { name: "banana" }, { name: "cherry"} ];
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// Map
+
+// const map = new Map();
+// map.set("IN", "India");
+// map.set("US", "United States");
+// console.log(map);
+
+// // Will not work as map is not an array
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+// for (const [key, value] of map) {
+//     console.log(key, value);
+// }
+
+
+// Iterate over objects
+
+// const obj = {
+//     name: "apple",
+//     price: 100
+// };
+
+// for (const [key, value] of Object.entries(obj)) {
+//     console.log(`Key -> ${key}, Value -> ${value}`);
+// }
+
+// for (const key in obj) {
+//     console.log(obj[key]);
+// }
+
+// const arr = ["apple", "banana", "cherry"];
+
+// console.log(arr.forEach((item) => { console.log(item); }));
+
+// function print(data) {
+//     console.log(data);
+// }
+
+// arr.forEach(print);
+
+// const code = [
+//     {
+//         name: "IN",
+//         country: "India"
+//     },
+//     {
+//         name: "US",
+//         country: "United States"
+//     }
+// ];
+
+// code.forEach((item) => {
+//     console.log(item.name);
+// }); 
+
